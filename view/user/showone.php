9644d19cb6870e47881f39876f9c5954a780c711
jfsantiago2 <jfsantiago2@esei.uvigo.es>
2016-11-18 22:19:36 +0100

    Añadir estilo al detalle de usuarios.

--- view/user/showone.php
+++ view/user/showone.php
@@ -12,28 +12,69 @@ $errors = $view->getVariable("errors");
 
 <div class="jumbotron">
     <div class="container">
-	  <div class="row">
-        <?php if($user->getFoto()){?>
-          <div class="col-sm-4 col-sm-push-8"><img src="/IU/pictures/<?= $user->getFoto()?>" alt="<?= $user->getFoto()?>" width="100px" height="100px" /></div>
-        <?php } else { ?>
-          <div class="col-sm-4 col-sm-push-8"><img src="/IU/pictures/profile_image.png" alt="default_image" width="100px" height="100px" /></div>
-        <?php } ?>
-        <div class="col-sm-8 col-sm-pull-4"><h1><?= i18n("User")?></h1></div>
+  	  <div class="row">
+          <?php if($user->getFoto()){?>
+            <div class="col-sm-4 col-sm-push-8"><img src="/IU/pictures/<?= $user->getFoto()?>" alt="<?= $user->getFoto()?>" width="100px" height="100px" /></div>
+          <?php } else { ?>
+            <div class="col-sm-4 col-sm-push-8"><img src="/IU/pictures/profile_image.png" alt="default_image" width="100px" height="100px" /></div>
+          <?php } ?>
+      <div class="col-sm-8 col-sm-pull-4"><h1><?= i18n("User")?></h1></div>
       </div>
-		<div class="row">
-		  <div>ID: <?= $user->getID() ?></div>
-		  <div>Profile: <?= $user->getProfile() ?></div>
-		  <div>DNI: <?= $user->getDni() ?></div>
-		  <div >Nombre usuario: <?= $user->getUsername() ?></div>
-		  <div>Nombre: <?= $user->getName() ?></div>
-		  <div>Apellidos: <?= $user->getSurname() ?></div>
-		  <div>Fecha nacimiento: <?= $user->getFechaNac() ?></div>
-		  <div>Direccion: <?= $user->getDireccion() ?></div>
-		  <div>Comentario: <?= $user->getComentario() ?></div>
-		  <div>Número cuenta: <?= $user->getNumCuenta() ?></div>
-		  <div>Tipo contrato: <?= $user->getTipoContrato() ?></div>
-		  <div>Email: <?= $user->getEmail() ?></div>
-		  <div>Activo: <?= $user->getActivo() ?></div>
-		</div>
+      <div class="row top-buffer">
+    		<table class="table" >
+          <tr class="active">
+            <th>ID:</th>
+            <td><?= $user->getID() ?></td>
+          </tr>
+          <tr>
+            <th>Profile:</th>
+            <td><?= $user->getProfile() ?></td>
+          </tr>
+          <tr class="active">
+            <th>DNI:</th>
+            <td><?= $user->getDni() ?></td>
+          </tr>
+          <tr>
+            <th>Nombre usuario:</th>
+            <td><?= $user->getUsername() ?></td>
+          </tr>
+          <tr class="active">
+            <th>Nombre:</th>
+            <td><?= $user->getName() ?></td>
+          </tr>
+          <tr>
+            <th>Apellidos:</th>
+            <td><?= $user->getSurname() ?></td>
+          </tr>
+          <tr class="active">
+            <th>Email:</th>
+            <td><?= $user->getEmail() ?></td>
+          </tr>
+          <tr>
+            <th>Fecha nacimiento:</th>
+            <td><?= $user->getFechaNac() ?></td>
+          </tr>
+          <tr class="active">
+            <th>Direccion:</th>
+            <td><?= $user->getDireccion() ?></td>
+          </tr>
+          <tr>
+            <th>Número cuenta:</th>
+            <td><?= $user->getNumCuenta() ?></td>
+          </tr>
+          <tr class="active">
+            <th>Tipo contrato:</th>
+            <td><?= $user->getTipoContrato() ?></td>
+          </tr>
+          <tr>
+            <th>Activo:</th>
+            <td><?= $user->getActivo() ?></td>
+          </tr>
+          <tr class="active">
+            <th class="col-md-2">Comentario:</th>
+            <td class="col-md-2"><?= $user->getComentario() ?></td>
+          </tr>
+        </table>
+      </div>
     </div>
 </div>
