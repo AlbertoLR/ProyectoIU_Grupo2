85d60204a2871a24ff98be550685548333854922
jfsantiago2 <jfsantiago2@esei.uvigo.es>
2016-11-20 21:14:12 +0100

    Last changes.

--- js/filter_users.js
+++ js/filter_users.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         local: users
     });
 
-    $('.typeahead').typeahead({
+    $('.search-typeahead').typeahead({
         hint: true,
         highlight: true,
         minLength: 1
