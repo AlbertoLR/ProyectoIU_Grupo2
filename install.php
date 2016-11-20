d0b1ac1a4e92c3b2e25ea703e551350ca35f4a78
hacklego <igsalgado@esei.uvigo.es>
2016-11-20 22:54:54 +0100

    Form added to install.php

--- install.php
+++ install.php
@@ -1,21 +1,30 @@
+<form action="install.php" method="POST">
+    <p>Mysql Root User: <input type="text" name="user" /></p>
+    <p>Mysql Root Password:  <input type="password" name="passwd" /></p>
+    <p><input type="submit" name="submit" value="submit" /></p>
+</form>
+
 <?php
-$mysqlUserName = 'root';
-$mysqlPasswd = 'iu';
-$dbFile = "moovett.sql";
-$command='mysql -u' .$mysqlUserName .' -p' .$mysqlPasswd . ' < ' .$dbFile;
-exec($command,$output=array(),$worked);
-switch($worked){
-    case 0:
-        echo 'Import file <b>' .$dbFile .'</b> successfully imported to database<br />';
-        echo 'Database user created: moovett with password moovett<br />';
-        echo 'Test user create: admin with password admin<br />';
-        echo '<a href="index.php">Proceed to Website</a><br />';
-        break;
-    case 1:
-        echo 'There was an error during import. User already Exists<br />';
-        echo 'Database user created: moovett with password moovett<br />';
-        echo 'Website user admin create: admin with password admin<br />';
-        echo '<a href="index.php">Proceed to Website</a><br />';
-        break;
+if (isset($_POST["submit"])){
+
+    $mysqlUserName = $_POST["user"];
+    $mysqlPasswd = $_POST["passwd"];
+    $dbFile = "moovett.sql";
+    $command='mysql -u' .$mysqlUserName .' -p' .$mysqlPasswd . ' < ' .$dbFile;
+    exec($command,$output=array(),$worked);
+    switch($worked){
+	case 0:
+            echo 'Import file <b>' .$dbFile .'</b> successfully imported to database<br />';
+            echo 'Database user created: moovett with password moovett<br />';
+            echo 'Test user created: admin with password admin<br />';
+            echo '<a href="index.php">Proceed to Website</a><br />';
+            break;
+	case 1:
+            echo 'There was an error during import: User already Exists or You insert bad credentials<br /><br />';
+            echo 'Database user created: moovett with password moovett<br /><br />';
+            echo 'Website user admin created: admin with password admin<br /><br />';
+            echo '<a href="index.php">Check if credentials are ok and Proceed to Website</a>';
+            break;
+    }
 }
 ?>
